4f81e1b74454931ebb924b2132fcb6b8abe87746
DaikiKosaka <tubuan.ankoro@gmail.com>
2024-09-23 16:16:38 +0900

    更新 20240923

--- test/src/test01/cytech01.java
+++ test/src/test01/cytech01.java
@@ -3,8 +3,34 @@ package test01;
 public class cytech01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World");
+		//設問1
+		int num = 100;
+		String text = "あいうえお";
+		boolean flag = true;
+		
+		System.out.println("num:" + num);
+		System.out.println("text:" + text);
+		System.out.println("flag:" + flag);
+		
+		//設問2
+		int number1 = 10;
+		int number2 = 50;
+		
+		int addition = number1 + number2;
+		System.out.println("加算" + addition);
+		
+		int subtract = number1 - number2;
+		System.out.println("減算" + subtract);
+		
+		int multiplication = number1 * number2;
+		System.out.println("乗算" + multiplication);
+		
+		int division = number1 % number2;
+		System.out.println("除算" + division);
+		
+		int amari = number1 / number2;
+		System.out.println("剰余算" + amari);
+
 
 	}
 
